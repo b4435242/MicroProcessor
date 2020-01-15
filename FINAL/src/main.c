4b061b8f3719c0a47f4215ed06c4ce1d0afa8f14
unknown <franktseng0718@gmail.com>
2020-01-15 13:47:41 +0800

    d

--- FINAL/src/main.c
+++ FINAL/src/main.c
@@ -2,8 +2,42 @@
 #include "utility.h"
 
 #define VALID_DISTANCE 20
-
+#define TIME_SEC 5
 int Distance;
+
+void timer_init()
+{
+	RCC->APB1ENR1 |= 0b1;
+	TIM3->ARR = (uint32_t) (TIME_SEC * (4000000 / 40000)); // reload value
+	TIM3->PSC = (uint32_t) 39999; // prescaler
+	TIM3->EGR = TIM_EGR_UG; // reinitialize the counter
+}
+
+void timer_start()
+{
+	TIM3->CR1 |= TIM_CR1_CEN;
+	display(0, -1003);
+	if (TIME_SEC <= 0 || TIME_SEC > 10000)
+	{
+		TIM3->CR1 &= ~TIM_CR1_CEN;
+		return;
+	}
+	int pre_val = 0;
+	while (1)
+	{
+		int now_val = TIM3->CNT;
+		if (pre_val > now_val)
+		{
+			TIM3->CR1 &= ~TIM_CR1_CEN;
+			return;
+		}
+		pre_val = now_val;
+
+
+
+		//display(now_val, -1000 - len);
+	}
+}
 
 int main(void) {
 	gpio_init();
@@ -24,8 +58,9 @@ int main(void) {
 
 		while(HCSR04GetDistance(0,1)<VALID_DISTANCE);
 
-		Delay_Ms((uint16_t) 80);
-
+		//Delay_Ms((uint16_t) 80);
+		timer_init();
+		timer_start();
 		sg90_move(65);
 		Delay_Ms((uint16_t) 10);
 		max7219_init();
